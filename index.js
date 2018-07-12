9135a040c506e09fb89508f7b2e6a89eb155c14f
Vishal Asai <asai_vishal@yahoo.com>
2018-07-12 14:18:01 +1000

    [#157238982] updated browser detection script to use new webpack 4 hooks

--- index.js
+++ index.js
@@ -20,43 +20,42 @@ function InjectPreloader(options) {
 
 InjectPreloader.prototype.apply = function (compiler) {
   var self = this;
-
-  compiler.plugin('compilation', function (compilation) {
-
-    compilation.plugin('html-webpack-plugin-before-html-processing', function (htmlPluginData, callback) {
-
-      var loadFile = function (path) {
-        return new RSVP.Promise(function (resolve, reject) {
-          fs.readFile (path, 'utf8', function (error, data) {
-            if (error) {
-              reject(error);
-            }
-            resolve(data);
+  compiler.hooks.compilation.tap('InjectPreloader', (compilation) => {
+    compilation.hooks.htmlWebpackPluginBeforeHtmlProcessing.tapAsync(
+      'InjectPreloader',
+      (htmlPluginData, callback) => {
+        var loadFile = function (path) {
+          return new RSVP.Promise(function (resolve, reject) {
+            fs.readFile (path, 'utf8', function (error, data) {
+              if (error) {
+                reject(error);
+              }
+              resolve(data);
+            });
           });
+        };
+
+        var promises = {
+          browserIndicator: loadFile(self.options.browserIndicatorTemplate),
+          loadIndicator: loadFile(self.options.loadIndicatorTemplate),
+          browserScript: loadFile(self.options.browserCheckScriptTemplate)
+        };
+
+        RSVP.hash(promises).then(function(files) {
+
+          htmlPluginData.html = htmlPluginData.html.replace(self.options.browserIndicatorPlaceholder, files.browserIndicator);
+          htmlPluginData.html = htmlPluginData.html.replace(self.options.loadIndicatorPlaceholder, files.loadIndicator);
+          htmlPluginData.html = htmlPluginData.html.replace(self.options.browserUpdateScriptPlaceholder, files.browserScript);
+
+          callback(null, htmlPluginData);
+
+        }).catch(function(reason) {
+          reason.plugin = 'preloader-html-webpack-plugin';
+          console.error(reason);
+          callback(reason, htmlPluginData);
         });
-      };
-
-      var promises = {
-        browserIndicator: loadFile(self.options.browserIndicatorTemplate),
-        loadIndicator: loadFile(self.options.loadIndicatorTemplate),
-        browserScript: loadFile(self.options.browserCheckScriptTemplate)
-      };
-
-      RSVP.hash(promises).then(function(files) {
-
-        htmlPluginData.html = htmlPluginData.html.replace(self.options.browserIndicatorPlaceholder, files.browserIndicator);
-        htmlPluginData.html = htmlPluginData.html.replace(self.options.loadIndicatorPlaceholder, files.loadIndicator);
-        htmlPluginData.html = htmlPluginData.html.replace(self.options.browserUpdateScriptPlaceholder, files.browserScript);
-
-        callback(null, htmlPluginData);
-
-      }).catch(function(reason) {
-        reason.plugin = 'preloader-html-webpack-plugin';
-        console.error(reason);
-        callback(reason, htmlPluginData);
-      });
-
-    });
+      }
+    );
   });
 };
 
